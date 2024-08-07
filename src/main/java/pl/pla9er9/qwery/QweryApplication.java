fdbd96171320ea0fefaffc17141de3da95de68ab
Pla9er <drugiekonto_99@o2.pl>
2024-08-07 19:18:54 +0200

    init java spring project

---
+++ src/main/java/pl/pla9er9/qwery/QweryApplication.java
@@ -0,0 +1,13 @@
+package pl.pla9er9.qwery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pla9er9/qwery/QweryApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pla9er9.qwery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
