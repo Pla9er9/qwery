8c79906b3e0236864ba834752f93191222e7775e
Pla9er <drugiekonto_99@o2.pl>
2024-08-15 00:04:53 +0200

    create simple controller

---
+++ src/main/java/pl/pla9er9/qwery/ApplicationController.java
@@ -0,0 +1,23 @@
+package pl.pla9er9.qwery;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApplicationController {
+
+    private final Trie trie;
+
+    public ApplicationController(Trie trie) {
+        this.trie = trie;
+    }
+
+    @GetMapping("/search")
+    public String[] search(
+            @RequestParam String value,
+            @RequestParam(required = false, defaultValue = "4") int limit) {
+
+        return trie.search(value, limit);
+    }
+}
