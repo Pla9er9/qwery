2b3b4e6fc5a6eac0fa462c00f21dc8a54289f257
Pla9er <drugiekonto_99@o2.pl>
2024-08-29 18:24:35 +0200

    Add controllers methods

--- src/main/java/pl/pla9er9/qwery/ApplicationController.java
+++ src/main/java/pl/pla9er9/qwery/ApplicationController.java
@@ -1,8 +1,8 @@
 package pl.pla9er9.qwery;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class ApplicationController {
@@ -13,11 +13,41 @@ public class ApplicationController {
         this.trie = trie;
     }
 
-    @GetMapping("/search")
+    @GetMapping("records/search")
     public String[] search(
             @RequestParam String value,
             @RequestParam(required = false, defaultValue = "10") int limit) {
 
         return trie.search(value, limit);
+    }
+
+    @PostMapping("records")
+    public String addRecord(@RequestParam String value) {
+        trie.add(value);
+        return value;
+    }
+
+    @DeleteMapping("records/v/{value}")
+    public String deleteRecord(@PathVariable String value) {
+        var result = trie.search(value, 1);
+        var found = result.length == 1;
+
+        if (!found) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+
+        trie.delete(value);
+
+        return result[0];
+    }
+
+    @GetMapping("records/all")
+    public String[] getAllRecords() {
+        return trie.getAll();
+    }
+
+    @DeleteMapping("records/all")
+    public void deleteAllRecords() {
+        trie.deleteAll();
     }
 }
